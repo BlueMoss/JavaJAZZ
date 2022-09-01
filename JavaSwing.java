d8390950e4d18896a8a092112a346a7062315890
akdldoal98 <akdldoal98@naver.com>
2022-09-01 12:57:08 +0900

    JavaJAZZ

---
+++ JavaSwing.java
@@ -0,0 +1,13 @@
+// package sec02;
+
+import javax.swing.JFrame;
+
+public class JavaSwing {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+
+        f.setTitle("오세빈! ");
+        f.setSize(1000, 600);
+        f.setVisible(true);
+    }
+}
